d3458c3cc74f885cb28dec49cf538a94354f3dc9
Marko Tiosavljevic <marko.tiosavljevic@gmail.com>
2025-07-09 04:54:45 +0200

    Update woocommerce-mautic-connector.php

--- woocommerce-mautic-connector.php
+++ woocommerce-mautic-connector.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce to Mautic Integration
  * Description: Simple integration to send WooCommerce order data to Mautic
  * Version: 1.0.0
- * Author: Your Name
+ * Author: Marko Tiosavljevic
  */
 
 // Prevent direct access
